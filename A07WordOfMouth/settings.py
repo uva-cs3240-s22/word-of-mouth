84c29a67768e276d1af7079e3071d9f2ee2634bf
bmselewski <bmselewski@gmail.com>
2022-02-25 18:44:26 -0500

    base django project heroku push

--- A07WordOfMouth/settings.py
+++ A07WordOfMouth/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5'
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
