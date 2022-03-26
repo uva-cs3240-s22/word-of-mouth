28924c7f8400958b815281a7730d30ea76206a9c
bmselewski <bmselewski@gmail.com>
2022-03-26 15:55:35 -0400

    added recipe model and model form. Use /recipe and /recipe/list

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('recipe/', views.recipe, name='recipe'),
+    path('recipe/list', views.RecipeList.as_view(), name='recipe_list')
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from .models import Recipe, RecipeForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
 
 
 class Index(TemplateView):
@@ -10,3 +13,21 @@ class Index(TemplateView):
 
     def get_context_data(self,*args, **kwargs):
         return {"user": self.request.user}
+
+
+def recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('')
+    else:
+        form = RecipeForm()
+    return render(request, 'main/recipe.html', {'form':form})
+
+class RecipeList(generic.ListView):
+    template_name = 'main/recipeList.html'
+    context_object_name = 'recipe_list'
+
+    def get_queryset(self):
+        return Recipe.objects.all()
