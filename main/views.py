fff0f253e86659183dd85483079db5b8bfc97955
Marcus Mann <mpm7cf@virginia.edu>
2022-03-26 12:32:55 -0400

    Added user login and redirect so it goes straight to Google's list of your google users

--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
@@ -9,4 +6,8 @@ class Index(TemplateView):
     template_name = 'main/index.html'
 
     def get_context_data(self,*args, **kwargs):
-        return {"user": self.request.user}
+        username = self.request.user.username if self.request.user.is_authenticated else "Not logged in"
+        x = super(Index, self).get_context_data(**kwargs)
+        x['user'] = self.request.user
+        x['username'] = username
+        return x
