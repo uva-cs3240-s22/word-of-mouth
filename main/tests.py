6ad28235d3dcd30e1866739f6fa8872279b4696c
jet9szu <jet9szu@virginia.edu>
2022-03-27 13:39:41 -0400

    Updated tests.py to include simple tests for the website itself.

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .views import Index
+
 
 # Create your tests here.
+class Index_tests(TestCase):
+
+    # Setting up
+    def setUp(self):
+        self.rf = RequestFactory()
+
+    # Testing that the website works
+    def Main_page_basic_test(self):
+        request = self.rf.get('/')
+        request.user = User.objects.create(id=1, username="Tester")
+        response = Index.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+
+    # Testing another user works
+    def Main_page_another_basic_test(self):
+        request = self.rf.get('/')
+        request.user = User.objects.create(id=2, username="Jard")
+        response = Index.as_view()(request)
+        self.assertEqual(response.status_code, 200)
