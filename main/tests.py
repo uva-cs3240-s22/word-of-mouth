5aa7eac6da2001d2f5c694024dcebdf8e636c9b5
Marcus Mann <mpm7cf@virginia.edu>
2022-03-27 15:55:58 -0400

    Added more tests

--- main/tests.py
+++ main/tests.py
@@ -1,26 +1,47 @@
-from django.test import RequestFactory, TestCase
-from django.contrib.auth.models import User
-from django.urls import reverse
-from .views import Index
+from datetime import datetime
 
+from allauth.socialaccount.models import SocialApp, SocialLogin, SocialAccount
+from django.contrib.auth.models import User, AnonymousUser
+from django.contrib.sites.models import Site
+from django.template.response import TemplateResponse
+from django.test import RequestFactory, TestCase
 
 # Create your tests here.
-class Index_tests(TestCase):
+from main.views import IndexView
+
+
+class IndexTests(TestCase):
 
     # Setting up
     def setUp(self):
         self.rf = RequestFactory()
+        self.request = self.rf.get('/')
 
     # Testing that the website works
-    def Main_page_basic_test(self):
-        request = self.rf.get('/')
-        request.user = User.objects.create(id=1, username="Tester")
-        response = Index.as_view()(request)
+    def test_user(self):
+        self.request.user = User.objects.create(id=1, username="Tester")
+        response = IndexView.as_view()(self.request)
         self.assertEqual(response.status_code, 200)
 
     # Testing another user works
-    def Main_page_another_basic_test(self):
-        request = self.rf.get('/')
-        request.user = User.objects.create(id=2, username="Jard")
-        response = Index.as_view()(request)
+    def test_user_2(self):
+        self.request.user = User.objects.create(id=2, username="Jard")
+        response = IndexView.as_view()(self.request)
         self.assertEqual(response.status_code, 200)
+
+    def test_with_anon_user(self):
+        self.request.user = AnonymousUser()
+        response = IndexView.as_view()(self.request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_with_google_user(self):
+        self.request.user = User.objects.create_user('google', 'google@g.com', '123')
+        SocialAccount.objects.create(user=self.request.user, provider='google', uid=123, last_login=datetime.now(), date_joined=datetime.now(), extra_data={"picture": "test.jpg"})
+        self.current_site = Site.objects.get_current()
+
+        response: TemplateResponse = IndexView.as_view()(self.request)
+
+        self.assertEqual(response.context_data['avatar_url'], 'test.jpg')
+
+
