a44abd477bce5d2ff1df7feba629c8f0d6aa3e40
my6jdq <my6jdq@gmail.com>
2022-04-09 16:32:14 -0400

    working on the actual forking, little progress

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from allauth.socialaccount.models import SocialAccount
+from django.forms import model_to_dict
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import TemplateView, FormView, CreateView
@@ -37,12 +38,18 @@ class RecipeCreateView(BaseMixin, CreateView):
     template_name = 'main/recipe.html'
     model = Recipe
     fields = ['title_text', 'ingredients_list', 'body_text']
+
+    def get_initial(self):
+        if 'from' in self.request.GET:
+            return model_to_dict(Recipe.objects.get(id=self.request.GET['from']), fields=self.fields)
+        else:
+            return self.initial
+
     success_url = reverse_lazy('recipe_list')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
-
 
 
 class RecipeListView(BaseMixin, generic.ListView):
