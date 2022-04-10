dde23ecb42a17bebd998295a41033f99f1170e65
my6jdq <my6jdq@gmail.com>
2022-04-10 03:16:59 -0400

    stuck on getting the url path correct

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('recipe/add', views.RecipeCreateView.as_view(), name='new_recipe'),
     path('recipe/list', views.RecipeListView.as_view(), name='recipe_list'),
+    # path('recipe/fork', views.RecipeCreateView.get_initial(), name='fork_recipe'),
 ]
